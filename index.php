f9d00016df942efded2f2b80dde56aae366158d0
nikosIos <nikos_ios@outlook.com>
2015-05-21 00:37:41 +0300

    1

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,20 +6,15 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <link rel="icon" href="../../favicon.ico">
-
         <title>Carousel Template for Bootstrap</title>
-
         <!-- Bootstrap core CSS -->
         <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-
-
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-
         <!-- Custom styles for this template -->
         <link href="carousel.css" rel="stylesheet">
     </head>
@@ -30,13 +23,11 @@
     <body>
         <div class="navbar-wrapper">
             <div class="container">
-
                 <nav class="navbar navbar-inverse navbar-static-top">
                     <div class="container">
                         <div class="navbar-header">
                             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                                 <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
@@ -63,11 +54,8 @@
                         </div>
                     </div>
                 </nav>
-
             </div>
         </div>
-
-
         <!-- Carousel
         ================================================== -->
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -120,7 +108,6 @@
             </a>
         </div><!-- /.carousel -->
 
-
         <!-- Marketing messaging and featurettes
         ================================================== -->
         <!-- Wrap the rest of the page in another container to center all the content. -->
@@ -148,7 +135,6 @@
                     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
                 </div><!-- /.col-lg-4 -->
             </div><!-- /.row -->
-
 
             <!-- START THE FEATURETTES -->
 
@@ -192,7 +178,6 @@
 
             <!-- /END THE FEATURETTES -->
 
-
             <!-- FOOTER -->
             <footer>
                 <p class="pull-right"><a href="#">Back to top</a></p>
@@ -200,7 +185,6 @@
             </footer>
 
         </div><!-- /.container -->
-
 
         <!-- Bootstrap core JavaScript
         ================================================== -->
@@ -211,4 +195,19 @@
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug 
         <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
     </body>
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+
+
+
+
+
+
+
+
+
+
 </html>
